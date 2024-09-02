9890a73be4b4b3c6112557fbcb566f7f2496f4a0
SR-LAB <ideathonbcmx@gmail.com>
2024-09-02 13:32:10 +0530

    1 day to go

---
+++ app/src/main/java/com/example/ecoflow/DashboardFragment.java
@@ -0,0 +1,20 @@
+package com.example.ecoflow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DashboardFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.dashboard_fragment, container, false);
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/ecoflow/MyProfileFragment.java
@@ -0,0 +1,19 @@
+package com.example.ecoflow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class MyProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.my_profile_fragment, container, false);
+
+        return root;
+    }
+}
