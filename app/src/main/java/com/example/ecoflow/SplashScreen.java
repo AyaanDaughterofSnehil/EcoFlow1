2495ef13cac6ed96d091a7e0cb1ff8698d76589a
SR-LAB <ideathonbcmx@gmail.com>
2024-09-04 09:58:13 +0530

    Last Day

--- app/src/main/java/com/example/ecoflow/SplashScreen.java
+++ app/src/main/java/com/example/ecoflow/SplashScreen.java
@@ -1,8 +1,13 @@
 package com.example.ecoflow;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,9 +15,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreen extends AppCompatActivity {
 
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        splashAnim();
+    }
+
+    private void splashAnim(){
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.fade);
+        imageView=findViewById(R.id.anim_logo);
+        anim.reset();
+        imageView.clearAnimation();
+        imageView.startAnimation(anim);
+
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
