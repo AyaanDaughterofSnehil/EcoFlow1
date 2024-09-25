3d048c692f824ad059cac0ccae355761818bb772
SR-LAB <ideathonbcmx@gmail.com>
2024-09-25 14:24:39 +0530

    Profile bana bsdk mc

---
+++ app/src/main/java/com/example/ecoflow/ConnectedThread.java
@@ -0,0 +1,87 @@
+package com.example.ecoflow;
+
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ConnectedThread extends Thread{
+    private static final String TAG = "FrugalLogs";
+    private final BluetoothSocket mmSocket;
+    private final InputStream mmInStream;
+    private final OutputStream mmOutStream;
+    private String valueRead;
+
+    public ConnectedThread(BluetoothSocket socket) {
+        mmSocket = socket;
+        InputStream tmpIn = null;
+        OutputStream tmpOut = null;
+
+        // Get the input and output streams; using temp objects because
+        // member streams are final.
+        try {
+            tmpIn = socket.getInputStream();
+        } catch (IOException e) {
+            Log.e(TAG, "Error occurred when creating input stream", e);
+        }
+        try {
+            tmpOut = socket.getOutputStream();
+        } catch (IOException e) {
+            Log.e(TAG, "Error occurred when creating output stream", e);
+        }
+        //Input and Output streams members of the class
+        //We wont use the Output stream of this project
+        mmInStream = tmpIn;
+        mmOutStream = tmpOut;
+    }
+
+    public String getValueRead(){
+        return valueRead;
+    }
+
+    public void run() {
+
+        byte[] buffer = new byte[1024];
+        int bytes = 0; // bytes returned from read()
+        int numberOfReadings = 0; //to control the number of readings from the Arduino
+
+        // Keep listening to the InputStream until an exception occurs.
+        //We just want to get 1 temperature readings from the Arduino
+        while (numberOfReadings < 1) {
+            try {
+
+                buffer[bytes] = (byte) mmInStream.read();
+                String readMessage;
+                // If I detect a "\n" means I already read a full measurement
+                if (buffer[bytes] == '\n') {
+                    readMessage = new String(buffer, 0, bytes);
+                    Log.e(TAG, readMessage);
+                    //Value to be read by the Observer streamed by the Obervable
+                    valueRead=readMessage;
+                    bytes = 0;
+                    numberOfReadings++;
+                } else {
+                    bytes++;
+                }
+
+            } catch (IOException e) {
+                Log.d(TAG, "Input stream was disconnected", e);
+                break;
+            }
+        }
+
+    }
+
+    // Call this method from the main activity to shut down the connection.
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not close the connect socket", e);
+        }
+    }
+
+}
